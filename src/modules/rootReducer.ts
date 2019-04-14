93cd5fed63105a20cc6f52e8fffcbc63c96eac8f
Ali Sloyan <vsloyan@movista.ru>
2019-04-14 20:16:06 +0300

    Удалил лишний редьюсер

--- src/modules/rootReducer.ts
+++ src/modules/rootReducer.ts
@@ -1,11 +1,9 @@
-import { userReducer } from "./user/userReducer";
 import { authReducer } from "./auth/authReducer";
 import { combineReducers } from "redux";
 import { firestoreReducer } from "redux-firestore";
 import { firebaseReducer } from "react-redux-firebase";
 
 export const rootReducer = combineReducers({
-  user: userReducer,
   auth: authReducer,
   fireStore: firestoreReducer,
   firebase: firebaseReducer
--- src/modules/user/userReducer.ts
+++
@@ -1,3 +0,0 @@
-export const userReducer = (initialState = {}, action: any) => {
-  return initialState;
-};
